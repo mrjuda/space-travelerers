a8ebeb952afd579c25854b4513e5307856cbd544
Joffrey NKESHIMANA <68817603+JoffreyNK@users.noreply.github.com>
2022-07-28 22:59:04 +0200

    Revert "Test missions"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
-import Navbar from './components/Header';
-import Missions from './components/Missions';
-import Rockets from './components/Rockets';
-import DragonsContainer from './components/DragonsContainer';
-import MyProfile from './components/MyProfile';
+import { Navbar } from './components/header';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/dragons" element={<DragonsContainer />} />
-          <Route path="/my-profile" element={<MyProfile />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
     </div>
   );
 }
--- src/redux/rockets/rocketsActions.js
+++
@@ -1,4 +0,0 @@
-// rocketsActions.js
-
-export const GET_ROCKETS = 'GET_ROCKETS';
-export const RESERVE_ROCKET = 'RESERVE_ROCKET';
